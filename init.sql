32a59bf649c753e4fa19b1144f4ab07174d8dc2c
Isaac Adamu <isaacmagaji1@gmail.com>
2019-12-31 16:48:51 +0100

    refactor(refactor code):refactor project code

--- init.sql
+++ init.sql
@@ -10,20 +10,20 @@ VALUES  ('Isaac'),
         ('Esson');
 
 
-CREATE TABLE public.user_tasks
+CREATE TABLE user_tasks
 (
     id SERIAL PRIMARY KEY,
     description VARCHAR(255) NOT NULL,
     state VARCHAR(50) NOT NULL,
     user_id integer NOT NULL,
-    CONSTRAINT user_tasks_pkey PRIMARY KEY (id),
     CONSTRAINT user_tasks_user_id_fkey FOREIGN KEY (user_id)
         REFERENCES public.users (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
 );
 
-INSERT INTO users (description, state, user_id )
+
+INSERT INTO user_tasks (description, state, user_id )
 VALUES  ('Do the dishes', 'done', 2),
         ('Write project reports', 'to do', 1);
     
